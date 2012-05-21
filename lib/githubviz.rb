b05fab3b05988fe775a06872862c16027c210206
JuliAne <juliane.doehler@googlemail.com>
2012-05-21 18:04:57 +0200

    added functions for displaying script languages for the whole network and added language legend for advanced settings

--- lib/githubviz.rb
+++ lib/githubviz.rb
@@ -29,13 +29,16 @@ end
 def aquire_data
    @api = GitHubV3API.new(ENV['GITHUB_API_KEY'])
    @data[@user] = @api.get("/users/#{@user}")
+         
 end
 
 def filter_data
+  
   @data[@user]['level'] = 0
   @data[@user]['follower_count'] = @data[@user]['followers']
   @data[@user]['followers'] = @api.get("/users/#{@user}/followers")
   @data[@user]['user'] = @api.get("/users/#{@user}")
+  
 end
 
 def get_data
@@ -50,6 +53,7 @@ def get_data
             t[f['login']]['follower_count'] = 0
             t[f['login']]['followers'] = @api.get("/users/#{f['login']}/followers")
             t[f['login']]['user'] = @api.get("/users/#{f['login']}")
+            
           end
         end
       end
@@ -61,9 +65,10 @@ def get_data
 end
 
 def process_data
+  
   @result['nodes'] = @data.keys
   @result['links'] = []
-
+  
   @data.each do |k,v|
     v['followers'].each do |f|
       @result['links'] << {
@@ -73,8 +78,72 @@ def process_data
       } if @result['nodes'].index(f['login'])
     end
   end
+  
+  @result['nodes'].map!{|n| {"name" => n, "group" => 1, "img" => @data[n]['avatar_url'], "profilseite" => @data[n]['user']['html_url'], "follower_count" => @data[n]['follower_count'], "repos" => @data[n]["repos"]}}
+  script_language
+end
 
-  @result['nodes'].map!{|n| {"name" => n, "group" => 1, "img" => @data[n]['avatar_url'], "profilseite" => @data[n]['user']['html_url'], "follower_count" => @data[n]['follower_count']}}
+def script_language
+  @result['nodes'].each do |user|
+    user_data = @api.users.get(user['name'])
+    page = 1
+    count = 30
+    user_repos = Array.new 
+      
+    while user_data.public_repos > count do
+      page = page +1
+      count = count + 30
+    end
+  
+    while page >= 1 do
+      user_repos[page-1] = @api.repos.list_repos(user['name'], page)
+      page = page - 1
+    end
+
+    @repos = user_repos
+    #get repo languages of a git user
+    @j = {}
+    start = 0
+    @j["repos"] = [] 
+    @j["repo_data"] = []
+    @j["language"] = []
+    @j["sort"] = {}
+    @j["max_lang"]=[]
+    @repos.each do |page|
+      page.each do |repo|
+        @j["repos"] << {"language"=>repo.language,"count" => 0}
+      end
+    end
+  
+    #remove doubles for comparing languages and count them
+    @j["repo_data"] = @j["repos"].uniq
+ 
+    #comparing languages of all repos and count them
+    @j["repo_data"].each do |language1|
+      @j["repos"].each do |language2|
+        if language1["language"] == language2["language"] then
+          language1["count"] += 1
+        end
+      end
+      @j["sort"].store(language1["language"], language1["count"])
+    end
+    @sort = @j["sort"].sort_by {|key, value| -value}
+    unless @sort.empty?
+      @sort.each do |scriptlanguage|
+        if scriptlanguage[1] == @sort[0][1] then
+          @j["max_lang"] << scriptlanguage[0]
+        end
+      end
+    else
+      @j["max_lang"] << "nothing"
+    end  
+    user['scriptlanguage'] = @j["max_lang"]
+  end
+  @scriptlanguage_legend = []
+  @result['nodes'].each do |user|
+    @scriptlanguage_legend << {"lang" => user['scriptlanguage'], "count" => 1}
+  end
+  @legend = @scriptlanguage_legend.uniq
 end
 
 def represent
@@ -90,18 +159,22 @@ get '/follower_viz' do
   @level = 0  
 
   @data = {}
-
+  
   @result = {"1" => "1"}
 
   @MAX_LEVELS = params[:level].to_i
 
   @user = params[:user]
   
+  @page = 1
+  @count = 30
+  @user_repos = Array.new 
+  
   if @user
     aquire_data
     filter_data
     get_data
-    process_data
+    process_data    
     represent
   end
 end
@@ -111,125 +184,7 @@ get '/repo_viz' do
 
   @api = GitHubV3API.new(ENV['GITHUB_API_KEY'])
  
-
   user_data = @api.users.get(@user)
-  #my_repos = @api.repos.list
-  page = 1
-  count = 30
-  user_repos = Array.new 
-      
-  while user_data.public_repos > count do
-   page = page +1
-   count = count + 30
-  end
-  
-  while page >= 1 do
-  user_repos[page-1] = @api.repos.list_repos(@user, page)
-  page = page - 1
-  end
-    
-  #a = 0 
-  #b = 0 
-  #c = 0
-  #repo_name = Array.new
-  #pr = Array.new
-  #pr_closed = Array.new
-  #pulldata = Array.new  
-  #closed_pulldata = Array.new 
-  #merge = ""
-  
-  # user_repos.each do |page|
-    # page.each do |repo|
-    # if repo.fork == true then
-       # a = a + 1  
-       # repo_name[a-1] = [repo.name, repo.source["owner"]["login"]]
-       # pr[a-1] = {"pr_repodata" => @api.pulls.list(repo.source["owner"]["login"], repo.name), "repoowner" => repo.source["owner"]["login"], "reponame" => repo.name}
-       # pr_closed[a-1] = {"closed_pr_repodata" => @api.pulls.list_closed(repo.source["owner"]["login"], repo.name), "repoowner" => repo.source["owner"]["login"], "reponame" => repo.name}
-    # else
-       # a = a
-    # end
-    # end
-  # end
-#   
- # pr.each do |pull|
-      # pull["pr_repodata"].each do |pullrequest|
-        # if pullrequest["user"]["login"] == @user then
-          # b = b + 1
-          # pulldata[b-1] = {"url" => pullrequest.url, "state" => pullrequest.state, "reponame" => pull["reponame"], "repoowner" => pull["repoowner"], "sum_pulls" => pull["pr_repodata"].length}
-        # else
-        # b = b
-        # end
-      # end
-    # end
-# 
-    # pr_closed.each do |closed_pull|
-      # closed_pull["closed_pr_repodata"].each do |closed_pullrequest|
-        # if closed_pullrequest["user"]["login"] == @user then
-          # c = c + 1
-          # #if closed_pullrequest.merged_at.nil? then
-          # # merge = ""
-          # #else
-          # # merge = closed_pullrequest.merged_at
-          # #end
-          # closed_pulldata[c-1] = {"title" => closed_pullrequest.title, "state" => closed_pullrequest.state, "created_at" => closed_pullrequest.created_at, "updated_at" => closed_pullrequest.updated_at, "closed_at" => closed_pullrequest.closed_at, "reponame" => closed_pull["reponame"], "repoowner" => closed_pull["repoowner"], "sum_pulls" => closed_pull["closed_pr_repodata"].length}
-        # else
-        # c = c
-        # end
-      # end
-    # end
-# 
-    # @pr_open = pr
-    # @number_of_forks = a
-    # @number_of_pulls = b
-    # @number_of_closed_pulls = c
-    # @number_of_all_pulls = b + c
-    # @pulldata = pulldata
-    # @closed_pulldata = closed_pulldata
-    # @reponame = repo_name
-  
-  
-  @repos = user_repos
-  
-  #get repo languages of a git user
-  @j = {}
-  start = 0
-  @j["repos"] = [] 
-  @j["repo_data"] = []
-  @j["language"] = []
-  @j["sort"] = {}
-  @repos.each do |page|
-    page.each do |repo|
-     @j["repos"] << {"language"=>repo.language,"count" => 0}
-    end
-  end
-  
-  #remove doubles for comparing languages and count them
-  @j["repo_data"] = @j["repos"].uniq
- 
-  #comparing languages of all repos and count them
-  @j["repo_data"].each do |language1|
-    @j["repos"].each do |language2|
-      if language1["language"] == language2["language"] then
-        language1["count"] += 1
-       
-      end
-    end
-    @j["sort"].store(language1["language"], language1["count"])
-  end
-  
-  @sort = @j["sort"].sort_by {|key, value| -value}
-  #@j["repo_data"].sort_by { |a| [ a.count] }
-  #building treshold for languages
-  
-  # treshold = 0.5
-  # base = @j["repos"].length 
-  # base.to_f
-  # @j["repo_data"].each do |language|
-#     
-    # if (language["count"].to_f / base) >= treshold then
-      # @j["language"] <<  language["language"]
-    # end
-  # end
   
   erb :repo
 end
